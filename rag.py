625073b09e4ef69eb906ae528305ab6b6425aafd
hoang98long <53008046+hoang98long@users.noreply.github.com>
2026-03-24 15:58:53 +0700

    Create rag.py

---
+++ rag.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def build_retriever():
+    loader = TextLoader("faq.txt")
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+
+    docs = splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    vectorstore = FAISS.from_documents(docs, embeddings)
+
+    return vectorstore.as_retriever()
